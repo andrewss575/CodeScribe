60a95d6ae692c875fa224ac787ae18ec2006a447
andrewss575 <78180011+andrewss575@users.noreply.github.com>
2024-10-21 21:19:18 -0700

    Initial Commit

---
+++ CodeScribe/CodeScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeScribeApp.swift
+//  CodeScribe
+//
+//  Created by Andrew Saldana on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
